99294aff58c7094cba1be93a470a52315c8d8353
Gabriel Pacheco Lopes <74369233+gabrielopes7@users.noreply.github.com>
2021-10-21 17:36:17 -0300

    Lista feita

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 // ​Crie uma lista de tarefas que tenha um input para receber a tarefa, e armazene na memória utilizando o LocalStorage.
 
 // Dica: Utilize Arrays e Json
+
+let texto = document.getElementById("text");
+let botao = document.getElementById("send");
+
+botao.addEventListener("click", add);
+
+function add() {
+  let criarLista = document.getElementsByClassName("criadorLista")[0];
+  let lista = document.createElement("li");
+  let linha = document.createElement("hr");
+
+  lista.append("elemento");
+  criarLista.appendChild(lista);
+  criarLista.appendChild(linha);
+}
