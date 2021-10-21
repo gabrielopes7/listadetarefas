c755b998d633c6388c9d35363268166b1770d55a
Gabriel Pacheco Lopes <74369233+gabrielopes7@users.noreply.github.com>
2021-10-21 19:37:57 -0300

    versãofinal

--- script.js
+++ script.js
@@ -1,30 +1,59 @@
-// ​Crie uma lista de tarefas que tenha um input para receber a tarefa, e armazene na memória utilizando o LocalStorage.
-
-// Dica: Utilize Arrays e Json
-
 let texto = document.getElementById("text");
 let botao = document.getElementById("send");
+let botaoEx = document.getElementById("all");
+let criaLista = document.getElementsByClassName("criadorLista")[0];
 
+let armazenamento = JSON.parse(localStorage.getItem("tarefas")) || [];
 
 botao.addEventListener("click", add);
+botaoEx.addEventListener("click", excluirTodos);
 
+function criarLista() {
+  criaLista.innerHTML = "";
 
+  for (elemento of armazenamento) {
+    let lista = document.createElement("li");
+    let texto = document.createTextNode(elemento);
 
-function armazenar(text){
-    localStorage.setItem("tarefas", JSON.stringify(text));
-    armazenamento.push(JSON.parse(localStorage.getItem("tarefas")));
-    localStorage.setItem("tarefas", JSON.stringify(armazenamento))
-}   
+    let linkExcluir = document.createElement("a");
+    let textoExcluir = document.createTextNode("Excluir");
+    let posF = armazenamento.indexOf(elemento);
+    linkExcluir.setAttribute("onclick", `excluir (${posF})`);
+    linkExcluir.setAttribute("href", "#");
+    linkExcluir.appendChild(textoExcluir);
 
+    lista.appendChild(texto);
+    lista.appendChild(linkExcluir);
+    criaLista.appendChild(lista);
+  }
+}
+
+criarLista();
 
 function add() {
-  let criarLista = document.getElementsByClassName("criadorLista")[0];
-  let lista = document.createElement("li");
-  let linha = document.createElement("hr");
+  if (texto.value == "") {
+    window.alert("Por favor, digite uma tarefa!");
+  } else {
+    armazenamento.push(texto.value);
+    texto.value = "";
+    texto.focus();
+    criarLista();
+    guardar();
+  }
+}
 
-  lista.append(texto.value);
-  criarLista.appendChild(lista);
-  criarLista.appendChild(linha);
+function excluir(pos) {
+  armazenamento.splice(pos, 1);
+  criarLista();
+  guardar();
+}
 
-  armazenar(texto.value);
+function excluirTodos() {
+  armazenamento = [];
+  criarLista();
+  guardar();
+}
+
+function guardar() {
+  localStorage.setItem("tarefas", JSON.stringify(armazenamento));
 }
