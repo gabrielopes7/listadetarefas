ebfd42284a61823a0f8d17c9e7eee911447da1e7
Gabriel Pacheco Lopes <74369233+gabrielopes7@users.noreply.github.com>
2021-10-21 18:20:07 -0300

    Avanço armazenamento

--- script.js
+++ script.js
@@ -5,14 +5,26 @@
 let texto = document.getElementById("text");
 let botao = document.getElementById("send");
 
+
 botao.addEventListener("click", add);
+
+
+
+function armazenar(text){
+    localStorage.setItem("tarefas", JSON.stringify(text));
+    armazenamento.push(JSON.parse(localStorage.getItem("tarefas")));
+    localStorage.setItem("tarefas", JSON.stringify(armazenamento))
+}   
+
 
 function add() {
   let criarLista = document.getElementsByClassName("criadorLista")[0];
   let lista = document.createElement("li");
   let linha = document.createElement("hr");
 
-  lista.append("elemento");
+  lista.append(texto.value);
   criarLista.appendChild(lista);
   criarLista.appendChild(linha);
+
+  armazenar(texto.value);
 }
