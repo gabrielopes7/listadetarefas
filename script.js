f1fab393744003f194c04c49c4622bb1b155625d
Gabriel Pacheco Lopes <74369233+gabrielopes7@users.noreply.github.com>
2021-10-21 19:44:03 -0300

    Comentários

--- script.js
+++ script.js
@@ -3,10 +3,12 @@ let botao = document.getElementById("send");
 let botaoEx = document.getElementById("all");
 let criaLista = document.getElementsByClassName("criadorLista")[0];
 
-let armazenamento = JSON.parse(localStorage.getItem("tarefas")) || [];
+let armazenamento = JSON.parse(localStorage.getItem("tarefas")) || []; // Essa variavel puxa os itens guardados no localStorage ou inicia como um array vazio.
 
 botao.addEventListener("click", add);
 botaoEx.addEventListener("click", excluirTodos);
+
+//Essa função abaixo, faz a renderização da lista, puxando o armazenamento e colocar os elementos da lista na tela.
 
 function criarLista() {
   criaLista.innerHTML = "";
@@ -28,8 +30,11 @@ function criarLista() {
   }
 }
 
-criarLista();
 
+criarLista(); // Faz a chamada da função na abertura da página.
+
+
+// Essa função adiciona o texto do input dentro do localStorage, faz teste de input vazio e chama a lista para ser renderizada e guarda o valor dentro do localStorage.
 function add() {
   if (texto.value == "") {
     window.alert("Por favor, digite uma tarefa!");
@@ -42,17 +47,22 @@ function add() {
   }
 }
 
+// Essa função pega o ultimo index do armazenamento no localStorage e exclui ela, chamando a renderização da lista e guardando a informação no localStorage novamente.
 function excluir(pos) {
   armazenamento.splice(pos, 1);
   criarLista();
   guardar();
 }
 
+
+// Essa função exclui tudo que há dentro do armazenamento , chamando a renderização da lista e guardando a nova informação no localStorage.
 function excluirTodos() {
   armazenamento = [];
   criarLista();
   guardar();
 }
+
+// Essa função exclusivamente é chamada para guardar as informações no localStorage.
 
 function guardar() {
   localStorage.setItem("tarefas", JSON.stringify(armazenamento));
